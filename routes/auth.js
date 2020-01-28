42209cc3d0c35ffa03f9980ae0566a612f1f6f6a
aasisrijal <aashishrijal84@gmail.com>
2020-01-28 23:28:40 +0545

    login and bcrypt added

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const { registerValidation, loginValidaation }= require('../validation')
+const { registerValidation, loginValidation } = require('../validation');
 
 
-//validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-	name: Joi.string().min(6).required(),
-	email: Joi.string().min(6).required().email(),
-	password: Joi.string().min(6).required()
-});
 
 
 router.post('/register', async (req, res) => {
@@ -23,22 +16,41 @@ router.post('/register', async (req, res) => {
 	const emailExists = await User.findOne({email: req.body.email})
 	if(emailExists) return res.status(400).send('email already esits');
 
+	//hash passwords
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//create new user
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password
+		password: hashedPassword
 	});
 	try{
 		const savedUser = await user.save();
-		res.send(savedUser);
+		// res.send(savedUser);
+		res.send({ user: user._id });
 
 	}catch(err){
 		res.status(400).send(err);
 	}
 });
 
-router.get('/hello', (req, res) => {
-	res.send('register router');
+
+//login
+router.post('/login', async (req, res) => {
+	//validate the data entered by user
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	//checking if the email exists
+	const user = await User.findOne({email: req.body.email})
+	if(!user) return res.status(400).send('email or password does not exists');
+
+	//pasword is correct or not
+	const validPass = await bcrypt.compare(req.body.password, user.password);
+	if(!validPass) return res.status(400).send('invalid password');
+	res.send('logged in');
 });
 
 module.exports = router;
